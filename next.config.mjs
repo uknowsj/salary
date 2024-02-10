5bba116e125fb2baf02036a00c0f650edcb1451e
uknowsj <uknowsj@gmail.com>
2024-02-10 22:23:11 +0900

    🎉 기본 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: false,
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
 };
 
 export default nextConfig;
