0f21fca3ada90b544039cc6db19392f29de2a28b
uknowsj <uknowsj@gmail.com>
2024-02-10 22:31:49 +0900

    💄 전역 폰트 적용(프리텐다드)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { fontPretendard } from '@/styles/fonts';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={fontPretendard.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
