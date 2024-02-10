45569039f7afd3c9afd4d5a8c2099e27ae568ac2
uknowsj <uknowsj@gmail.com>
2024-02-11 00:56:02 +0900

    💄 hits 통계 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,16 @@
 import React, { ReactNode } from 'react';
-
+import Image from 'next/image';
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<div className='bg-[#EEEEF3]'>
 			<div className='mx-auto flex min-h-svh max-w-[420px] flex-col items-center bg-white px-[20px] pt-8'>
 				{children}
+				{/* hits */}
+				<div className='mb-8'>
+					<a href='https://hits.sh/salary.devmua.com/'>
+						<img alt='Hits' src='https://hits.sh/salary.devmua.com.svg?color=4e75fa&labelColor=ffffff' />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
