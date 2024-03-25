5eab8f275e09db502025820c75685bb05078c1e2
uknowsj <uknowsj@gmail.com>
2024-03-26 03:39:09 +0900

    💄 layout에 이메일추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,21 @@
-import React, { ReactNode } from 'react';
-import Image from 'next/image';
+import { ReactNode } from 'react';
+import EmailIC from '@/assets/images/email.svg';
+
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<div className='bg-[#EEEEF3]'>
-			<div className='mx-auto flex min-h-svh max-w-[420px] flex-col items-center bg-white px-[20px] pt-8'>
+		<div className='bg-gray-100'>
+			<div className='mx-auto flex min-h-svh max-w-[420px] flex-col items-center bg-white px-5 pt-8'>
 				{children}
-				{/* hits */}
-				<div className='opacity-0'>
+				<footer className='flex flex-col items-center'>
+					{/* hits */}
 					<a href='https://hits.sh/salary.devmua.com/'>
 						<img alt='Hits' src='https://hits.sh/salary.devmua.com.svg?color=4e75fa&labelColor=ffffff' />
 					</a>
-				</div>
+					<div className='my-3 flex items-center gap-1'>
+						<EmailIC />
+						<p className='text-xs text-text-secondary'>devmua0707@gmail.com</p>
+					</div>
+				</footer>
 			</div>
 		</div>
 	);
