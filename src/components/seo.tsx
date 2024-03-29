ea93cd4b17ab2237d18a201db8be79df3ad5adab
uknowsj <uknowsj@gmail.com>
2024-03-29 22:17:06 +0900

    🔍 SEO 메타태그 변경

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -7,10 +7,12 @@ export default function SEO({ title, description, children }: SEOProps) {
 
 	return (
 		<Head>
+			{/* HTML Meta Tags */}
 			<title>재미로 비교해보는 내 연봉 위치</title>
 			<meta name='description' content='나는 친구보다 얼마나 더 벌고 있을까?' />
+
+			{/* Facebook Meta Tags */}
 			<meta property='og:type' content='website' />
-			<meta property='og:site_name' content='데브무아 연봉 비교 테스트' />
 			<meta property='og:url' content={`${process.env.NEXT_PUBLIC_SITE_URL}${route.asPath}`} />
 			<meta property='og:locale' content='ko_KR' />
 			<meta property='og:title' content={title} />
@@ -19,8 +21,14 @@ export default function SEO({ title, description, children }: SEOProps) {
 			<meta property='og:image:width' content='704' />
 			<meta property='og:image:height' content='352' />
 
-			{/* Twitter */}
+			{/* Twitter Meta Tags */}
 			<meta name='twitter:card' content='summary_large_image' />
+			<meta property='twitter:domain' content={`${process.env.NEXT_PUBLIC_SITE_URL}`} />
+			<meta property='twitter:url' content={`${process.env.NEXT_PUBLIC_SITE_URL}${route.asPath}`} />
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content='/thumbnail.webp' />
+
 			{children}
 		</Head>
 	);
