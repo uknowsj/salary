40f567e1b1c3804dee54eb36b4d58dc84dcaab33
uknowsj <uknowsj@gmail.com>
2024-03-30 01:35:05 +0900

    🔍 SEO 이미지 태그 변경

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -8,7 +8,7 @@ export default function SEO({ title, description, children }: SEOProps) {
 	return (
 		<Head>
 			{/* HTML Meta Tags */}
-			<title>재미로 비교해보는 내 연봉 위치</title>
+			<title>재미로 보는 내 연봉 위치</title>
 			<meta name='description' content='나는 친구보다 얼마나 더 벌고 있을까?' />
 
 			{/* Facebook Meta Tags */}
@@ -17,9 +17,7 @@ export default function SEO({ title, description, children }: SEOProps) {
 			<meta property='og:locale' content='ko_KR' />
 			<meta property='og:title' content={title} />
 			<meta property='og:description' content={description} />
-			<meta property='og:image' content='/thumbnail.webp' />
-			<meta property='og:image:width' content='704' />
-			<meta property='og:image:height' content='352' />
+			<meta property='og:image' content={`${process.env.NEXT_PUBLIC_SITE_URL}/thumbnail.webp`} />
 
 			{/* Twitter Meta Tags */}
 			<meta name='twitter:card' content='summary_large_image' />
@@ -27,7 +25,7 @@ export default function SEO({ title, description, children }: SEOProps) {
 			<meta property='twitter:url' content={`${process.env.NEXT_PUBLIC_SITE_URL}${route.asPath}`} />
 			<meta name='twitter:title' content={title} />
 			<meta name='twitter:description' content={description} />
-			<meta name='twitter:image' content='/thumbnail.webp' />
+			<meta name='twitter:image' content={`${process.env.NEXT_PUBLIC_SITE_URL}/thumbnail.webp`} />
 
 			{children}
 		</Head>
